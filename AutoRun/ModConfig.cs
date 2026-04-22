0b540c86293cb22cc595673a5e0b410e549324fa
luciesunder <lucie.sun@gmail.com>
2026-04-22 21:19:59 +0200

    handle player changing direction

--- AutoRun/ModConfig.cs
+++ AutoRun/ModConfig.cs
@@ -4,5 +4,5 @@ namespace AutoRun;
 
 public class ModConfig
 {
-    public KeybindList ToggleKey { get; set; } = KeybindList.Parse("MouseX1");
+    public KeybindList ToggleKey { get; set; } = KeybindList.Parse("LeftControl");
 }
--- AutoRun/ModEntry.cs
+++ AutoRun/ModEntry.cs
@@ -11,50 +11,52 @@ namespace AutoRun
     internal sealed class ModEntry : Mod
     {
         private ModConfig Config = null!;
-        private bool IsAutoRunning = false;
+        private bool IsAutoRunning;
         
         public override void Entry(IModHelper helper)
         {
             Config = helper.ReadConfig<ModConfig>();
-            helper.Events.Input.ButtonPressed += OnToggle;
-            helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+            helper.Events.GameLoop.UpdateTicking += OnUpdateTicking;
         }
-        
-        
-        private void OnToggle(object? sender, ButtonPressedEventArgs e)
+
+        private void OnUpdateTicking(object? sender, UpdateTickingEventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
+
+            // Handle toggle
+            if (Config.ToggleKey.JustPressed())
+            {
+                IsAutoRunning = !IsAutoRunning;
+            }
+
+            if (!IsAutoRunning)
+                return;
             
-            if (!Config.ToggleKey.JustPressed())
+            bool isChangingDirection =
+                Game1.options.moveUpButton.Any(button => Helper.Input.IsDown((SButton)button.key)) ||
+                Game1.options.moveDownButton.Any(button => Helper.Input.IsDown((SButton)button.key)) ||
+                Game1.options.moveLeftButton.Any(button => Helper.Input.IsDown((SButton)button.key)) ||
+                Game1.options.moveRightButton.Any(button => Helper.Input.IsDown((SButton)button.key));
+
+            if (isChangingDirection)
                 return;
 
-            IsAutoRunning = !IsAutoRunning;
-            string message = IsAutoRunning ? "activated" : "deactivated";
-            Monitor.Log(message, LogLevel.Debug);
-        }
+            int direction = Game1.player.facingDirection.Value;
+            var buttons = direction switch
+            {
+                0 => Game1.options.moveUpButton,
+                1 => Game1.options.moveRightButton,
+                2 => Game1.options.moveDownButton,
+                3 => Game1.options.moveLeftButton,
+                _ => null
+            };
 
-        private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e)
-        {
-            if (!Context.IsWorldReady || !IsAutoRunning)
-                return;
- 
-            Helper.Input.Press(SButton.Q);
-            
-            Rectangle box = Game1.player.GetBoundingBox();
-            Rectangle shifted = new Rectangle(box.X - 4, box.Y, box.Width, box.Height);
-            bool isBlocked = Game1.currentLocation.isCollidingPosition(
-                shifted,
-                Game1.viewport,
-                Game1.player
-            );
-            Monitor.Log($"isCollidingPosition: {isBlocked}", LogLevel.Debug);
-            
-            if (!isBlocked)
-                return;
-            
-            IsAutoRunning = false;
+            if (buttons != null && buttons.Length > 0)
+            {
+                SButton key = (SButton)buttons[0].key;
+                Helper.Input.Press(key);
+            }
         }
     }
 }
